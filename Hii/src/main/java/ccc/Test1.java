cb74995bff620729be5256ae3fc1bfd11008b328
TWL <1373738957@qq.com>
2019-05-09 00:30:36 +0800

    添加Test1

--- Hii/src/main/java/ccc/Test1.java
+++ Hii/src/main/java/ccc/Test1.java
@@ -1,7 +1,4 @@
 package ccc;
 
 public class Test1 {
-    public static void main(String[] args) {
-        System.out.println(1111);
-    }
 }
--- Hii/src/main/java/ccc/Test2.java
+++
@@ -1,7 +0,0 @@
-package ccc;
-
-public class Test2 {
-    public static void main(String[] args) {
-        System.out.println(123);
-    }
-}

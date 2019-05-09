5f9f04fa331e90cfcb82af0a9d6818975bb84cab
TWL <1373738957@qq.com>
2019-05-09 10:47:29 +0800

    提交i

--- Hii/src/main/java/ccc/Test1.java
+++ Hii/src/main/java/ccc/Test1.java
@@ -1,4 +1,10 @@
 package ccc;
 
 public class Test1 {
+    public static void main(String[] args) {
+        int i = 2147483647;
+        System.out.println(i);
+        int j = i+1;
+        System.out.println(j);
+    }
 }
